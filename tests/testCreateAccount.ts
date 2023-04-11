39daace2e979e0a82a2e6359bca3f419c36bab23
hoak <hoak@offpiste.io>
2023-04-11 03:23:07 +0100

    adding create whitelist account example

--- tests/testCreateAccount.ts
+++ tests/testCreateAccount.ts
@@ -1,6 +1,7 @@
 import { CypherAccount, CypherSubAccount } from '@cypher-client/accounts'
 import {
   deriveAccountAddress,
+  derivePrivateClearingAddress,
   derivePublicClearingAddress,
   encodeStrToUint8Array,
   getDefaultPriorityFeeIxs,
@@ -9,6 +10,7 @@ import { confirmOpts, loadWallet } from './utils'
 import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet'
 import { CypherClient } from '@cypher-client/client'
 import { Cluster } from '@cypher-client/types'
+import { PublicKey } from '@solana/web3.js'
 
 // Load  Env Variables
 require('dotenv').config({
@@ -98,3 +100,36 @@ export const createSubAcc = async () => {
     throw e
   }
 }
+
+export const createWhitelistedMasterAcc = async () => {
+  const wallet = loadWallet(KP_PATH)
+  const client = new CypherClient(CLUSTER, RPC_ENDPOINT, new NodeWallet(wallet), confirmOpts)
+  const authority = wallet.publicKey
+  const cypherPID = client.cypherPID
+
+  const [privateClearing] = derivePrivateClearingAddress(1, cypherPID)
+  console.log(privateClearing.toString())
+  const whitelistAccount = new PublicKey('DVser61Jg9PSWuBAhSmxhCiVBpjbJXLFzk4ESxvWc7DS')
+  console.log(whitelistAccount.toString())
+
+  const masterAccountData = await CypherAccount.createWhitelisted(
+    client,
+    privateClearing,
+    whitelistAccount,
+    authority,
+    0,
+  )
+
+  try {
+    const { modifyComputeUnits, addPriorityFee } = getDefaultPriorityFeeIxs()
+    await client.sendAndConfirmIxs(
+      [modifyComputeUnits, addPriorityFee, ...masterAccountData.ixs],
+      [wallet],
+    )
+  } catch (e) {
+    console.error('Failed to create master account', e)
+    throw e
+  }
+}
+
+createWhitelistedMasterAcc()
