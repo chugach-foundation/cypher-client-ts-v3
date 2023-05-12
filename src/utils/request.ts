396b8f0038a664f6bcc2b9296af4ff6be126ee6e
Mark Pradhan <mark.pradhan@gmail.com>
2023-05-12 19:41:43 +0200

    improved repeat when failed utility fn

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,23 +1,23 @@
 import { sleep } from './shared';
 
-export const repeatWhenFailed = <T extends unknown, Y extends unknown>(
-  call: (x: Y) => Promise<T>,
+export const repeatWhenFailed = <T extends unknown, Args extends unknown[]>(
+  call: (...args: Args) => Promise<T>,
   timeout: number,
-  fnName = 'Unknown asycn function call'
+  fnName = 'Unknown async function call'
 ) => {
-  return async (x: Y) => {
+  return async (...args: Args) => {
     try {
-      return await call(x);
+      return await call(...args);
     } catch (err) {
-      console.log(`${fnName} failed! repeating in ${timeout} ms...`);
+      console.log(`${fnName} call failed! repeating in ${timeout} ms...`);
       console.error(err);
       await sleep(timeout);
-      const repeater: (x: Y) => Promise<T> = repeatWhenFailed(
+      const repeater: (...args: Args) => Promise<T> = repeatWhenFailed(
         call,
         timeout,
         fnName
       );
-      return await repeater(x);
+      return await repeater(...args);
     }
   };
 };
